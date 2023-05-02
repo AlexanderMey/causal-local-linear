28b4d5a002d9d32a22b5b6b035deb15025de1514
Mey <a.mey@tue.nl>
2023-05-02 22:20:22 +0200

    changes

--- MainAlgo/Main_full.py
+++ MainAlgo/Main_full.py
@@ -7,29 +7,32 @@ from heapq import nlargest
 
 
 def main(x,y,B,l):
-    E,n,d=x.shape
+    E=len(x)
+    n,d=x[0].shape
     temp=[list(itertools.combinations(range(d), k)) for k in range(0,d)]
     subsets = [item for sublist in temp for item in sublist]
-    R=np.zeros((E,n))
     dic={}
     for ind in subsets:
+        R=[]
         for i in range(E):
+            n,d=x[i].shape
             if len(ind)==0:
-                R[i,:]=y[i,:]
+                R.append(y[i]-np.mean(y[i]))
             else:
+                xtemp=x[i][:,ind]
+                # xtemp=np.concatenate((xtemp,np.ones((n,1))),axis=1)
                 ind=np.array(ind)
-                beta_hat=np.linalg.inv(x[i,:,ind]@x[i,:,ind].T)@(x[i,:,ind])@y[i,:]
-                R[i,:]=y[i,:]-x[i,:,ind].T@beta_hat
+                beta_hat=np.linalg.inv(xtemp.T@xtemp)@(xtemp.T)@y[i]
+                R.append(y[i]-xtemp@beta_hat)
         dic[tuple(ind)]=copy.copy(R)
     S_ini=[]
     for k in dic:
         Res=dic[k]
         pval=0
+        n=len(Res)
         for b in range(B):
             a=np.random.chisquare(n-len(k),E)
-            T=np.sum(Res**2,axis=1)
-    #                 if np.min(T)/np.max(T)>np.min(a)/np.max(a):
-    #                     pval+=1/B
+            T=[np.sum(r**2) for r in Res]
             T_b=np.sort(a,kind='mergesort')
             T_data=np.sort(T,kind='mergesort')
             if np.sum(T_data[:l])/np.sum(T_data[-l:])>np.sum(T_b[:l])/np.sum(T_b[-l:]):
